d5c9ebaec886d63dbf2e53634ab3dd216f67eaaf
soumabasu <98078134+soumabasu@users.noreply.github.com>
2023-06-27 07:52:14 +0530

    Delete step-definitions directory

--- step-definitions/Given.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-const { Given } = require('@cucumber/cucumber'); 
-
-// Custom Step Definitions can be written in this file
--- step-definitions/mobile/mobile-when.js
+++
@@ -1,6 +0,0 @@
-const { When } = require('@cucumber/cucumber');
-const  elementActions  = require('../../utils/mobile-actions/mob-index')
-
-When(/^I click on "([^"]*)"/, { timeout: 2 * 60000 }, async function (locatorName) {
-  await elementActions.click(locatorName)
-});
