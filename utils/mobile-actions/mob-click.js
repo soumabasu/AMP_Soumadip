949f98e068507179ff6d36549953bcb1a3b8507e
soumadipbasupersonal <soumadipbasu333@gmail.com>
2023-07-28 11:31:27 +0530

    Added one  android feature file and update one function
    Added one  feature file and update one function regarding launch the settings app in android & iOS device.

--- utils/mobile-actions/mob-click.js
+++ utils/mobile-actions/mob-click.js
@@ -1,5 +1,6 @@
 const {logger} = require('../../logger/log4js');
 const { getElement } = require('../common-actions/find-element');
+const platform = process.env.PLATFORM_NAME
 
 module.exports = {
   click: async (locatorName) => {
@@ -12,4 +13,26 @@ module.exports = {
       throw new Error(`Clicking on the ${locatorName} failed - ${err}`);
     }
   },
-}
+
+  launchSettings: async () => {
+    try {
+      switch(platform){
+      case 'android':
+      await browser.startActivity('com.android.settings', 'com.android.settings.Settings')
+      logger.info('Pass : Device setting lauched successfully');
+      break;
+
+      case 'iOS':
+      await browser.execute('mobile: launchApp', { bundleId: 'com.apple.Preferences' })
+      logger.info('Pass : Device setting lauched successfully');
+      break;
+
+      default:
+      logger.fatal('platform specified doesnt match with ios or android');
+      break;
+    }}catch (error) {
+      console.error('Error launching settings:', error);
+    } 
+  }
+  
+}  
